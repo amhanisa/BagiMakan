fd5c18208ca382fefc733e0678c170858316fc09
Azka Muhammad <azka@amhanisa.com>
2020-04-29 15:25:40 +0700

    Update Gradle

--- app/src/main/java/com/example/bagimakan/FragmentHome.java
+++ app/src/main/java/com/example/bagimakan/FragmentHome.java
@@ -3,11 +3,20 @@ package com.example.bagimakan;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 /**
@@ -15,6 +24,9 @@ import android.view.ViewGroup;
  */
 public class FragmentHome extends Fragment {
 
+    FirebaseFirestore db;
+
+private TextView txtHome;
 
     public FragmentHome() {
         // Required empty public constructor
@@ -25,7 +37,28 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        //test firestore
+        db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                Log.d("ASD", document.getId() + " => " + document.getData());
+                            }
+                        } else{
+                            Log.e("ASD", "Error getting document", task.getException());
+                        }
+                    }
+                });
+
+        txtHome = view.findViewById(R.id.txtHome);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/bagimakan/FragmentTengah.java
+++ app/src/main/java/com/example/bagimakan/FragmentTengah.java
@@ -25,7 +25,10 @@ public class FragmentTengah extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tengah, container, false);
+        View view = inflater.inflate(R.layout.fragment_tengah, container, false);
+
+
+        return view;
     }
 
 }
